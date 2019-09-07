a13e207db4a9208a7833867289db7e84e643f991
tangjie133 <1334875503@qq.com>
2019-09-07 17:43:00 +0800

    1

---
+++ OLEDV1.ts
@@ -0,0 +1,211 @@
+//% color="#AA278D"  block="OLEDV1"
+namespace OLEDV1 {
+    //% weight=100
+    //% blockId=OLEDV1_INIT
+    //% block="INIT_oled"
+    export function initDisplay(): void {
+        cmd(0xAE);  // Set display OFF
+        cmd(0xD5);  // Set Display Clock Divide Ratio / OSC Frequency 0xD4
+        cmd(0x80);  // Display Clock Divide Ratio / OSC Frequency 
+        cmd(0xA8);  // Set Multiplex Ratio
+        cmd(0x3F);  // Multiplex Ratio for 128x64 (64-1)
+        cmd(0xD3);  // Set Display Offset
+        cmd(0x00);  // Display Offset
+        cmd(0x40);  // Set Display Start Line
+        cmd(0x8D);  // Set Charge Pump
+        cmd(0x14);  // Charge Pump (0x10 External, 0x14 Internal DC/DC)
+        cmd(0xA1);  // Set Segment Re-Map
+        cmd(0xC8);  // Set Com Output Scan Direction
+        cmd(0xDA);  // Set COM Hardware Configuration
+        cmd(0x12);  // COM Hardware Configuration
+        cmd(0x81);  // Set Contrast
+        cmd(0xCF);  // Contrast
+        cmd(0xD9);  // Set Pre-Charge Period
+        cmd(0xF1);  // Set Pre-Charge Period (0x22 External, 0xF1 Internal)
+        cmd(0xDB);  // Set VCOMH Deselect Level
+        cmd(0x40);  // VCOMH Deselect Level
+        cmd(0xA4);  // Set all pixels OFF
+        cmd(0xA6);  // Set display not inverted
+        cmd(0xAF);  // Set display On
+        clear();
+    }
+    //% weight=60
+    //% blockId=OLEDV1_Clear
+    //% block="clear"
+    export function clear() {
+        cmd(DISPLAY_OFF);   //display off
+        for (let j = 0; j < 8; j++) {
+            setText(j,0);
+            {
+                for (let i = 0; i < 16; i++)  //clear all columns
+                {
+                    putChar(' ');
+                }
+            }
+        }
+        cmd(DISPLAY_ON);    //display on
+        setText(0,0);
+    }
+    function setText(row: number, column: number) {
+        let r = row;
+        let c = column;
+        if (row < 0) { r = 0 }
+        if (column < 0) { c = 0 }
+        if (row > 7) { r = 7 }
+        if (column > 15) { c = 15 }
+
+        cmd(0xB0 + r);            //set page address
+        cmd(0x00 + (8 * c & 0x0F));  //set column lower address
+        cmd(0x10 + ((8 * c >> 4) & 0x0F));   //set column higher address
+    }
+    function putChar(c: string) {
+        let c1 = c.charCodeAt(0);
+        writeCustomChar(basicFont[c1 - 32]);
+    }
+    //% weight=90
+    //% blockId=OLEDV1_Text
+    //% text.defl="DFRobot"
+    //% line.min=0 line.max=7
+    //% block="OLED show line %line|text %text"
+    export function showUserText(line: number, text: string) {
+
+
+        setText(line,0);
+
+        for (let c of text) {
+            putChar(c);
+        }
+        for (let i = text.length ; i < 16 ;i++) {
+            setText(line, i);
+            putChar(" ");
+        }
+    }
+	/**
+     * OLED 12864 shows the number
+     * @param line line num (8 pixels per line), eg: 0
+     * @param n value , eg: 2019
+     */
+    //% weight=90
+    //% blockId=OLEDV1_Nunber
+    //% line.min=0 line.max=7
+    //% block="OLED show line %line|number %n"
+    export function showUserNumber(line: number, n: number) {
+        OLEDV1.showUserText(line, "" + n)
+    }
+
+    function writeCustomChar(c: string) {
+        for (let i = 0; i < 8; i++) {
+            writeData(c.charCodeAt(i));
+        }
+    }
+
+    function cmd(c: number) {
+        pins.i2cWriteNumber(0x3c, c, NumberFormat.UInt16BE);
+    }
+
+    function writeData(n: number) {
+        let b = n;
+        if (n < 0) { n = 0 }
+        if (n > 255) { n = 255 }
+        pins.i2cWriteNumber(0x3c, 0x4000 + b, NumberFormat.UInt16BE);
+    }
+    const DISPLAY_OFF = 0xAE;
+    const DISPLAY_ON = 0xAF;
+    const basicFont: string[] = [
+        "\x00\x00\x00\x00\x00\x00\x00\x00", // " "
+        "\x00\x00\x5F\x00\x00\x00\x00\x00", // "!"
+        "\x00\x00\x07\x00\x07\x00\x00\x00", // """
+        "\x00\x14\x7F\x14\x7F\x14\x00\x00", // "#"
+        "\x00\x24\x2A\x7F\x2A\x12\x00\x00", // "$"
+        "\x00\x23\x13\x08\x64\x62\x00\x00", // "%"
+        "\x00\x36\x49\x55\x22\x50\x00\x00", // "&"
+        "\x00\x00\x05\x03\x00\x00\x00\x00", // "'"
+        "\x00\x1C\x22\x41\x00\x00\x00\x00", // "("
+        "\x00\x41\x22\x1C\x00\x00\x00\x00", // ")"
+        "\x00\x08\x2A\x1C\x2A\x08\x00\x00", // "*"
+        "\x00\x08\x08\x3E\x08\x08\x00\x00", // "+"
+        "\x00\xA0\x60\x00\x00\x00\x00\x00", // ","
+        "\x00\x08\x08\x08\x08\x08\x00\x00", // "-"
+        "\x00\x60\x60\x00\x00\x00\x00\x00", // "."
+        "\x00\x20\x10\x08\x04\x02\x00\x00", // "/"
+        "\x00\x3E\x51\x49\x45\x3E\x00\x00", // "0"
+        "\x00\x00\x42\x7F\x40\x00\x00\x00", // "1"
+        "\x00\x62\x51\x49\x49\x46\x00\x00", // "2"
+        "\x00\x22\x41\x49\x49\x36\x00\x00", // "3"
+        "\x00\x18\x14\x12\x7F\x10\x00\x00", // "4"
+        "\x00\x27\x45\x45\x45\x39\x00\x00", // "5"
+        "\x00\x3C\x4A\x49\x49\x30\x00\x00", // "6"
+        "\x00\x01\x71\x09\x05\x03\x00\x00", // "7"
+        "\x00\x36\x49\x49\x49\x36\x00\x00", // "8"
+        "\x00\x06\x49\x49\x29\x1E\x00\x00", // "9"
+        "\x00\x00\x36\x36\x00\x00\x00\x00", // ":"
+        "\x00\x00\xAC\x6C\x00\x00\x00\x00", // ";"
+        "\x00\x08\x14\x22\x41\x00\x00\x00", // "<"
+        "\x00\x14\x14\x14\x14\x14\x00\x00", // "="
+        "\x00\x41\x22\x14\x08\x00\x00\x00", // ">"
+        "\x00\x02\x01\x51\x09\x06\x00\x00", // "?"
+        "\x00\x32\x49\x79\x41\x3E\x00\x00", // "@"
+        "\x00\x7E\x09\x09\x09\x7E\x00\x00", // "A"
+        "\x00\x7F\x49\x49\x49\x36\x00\x00", // "B"
+        "\x00\x3E\x41\x41\x41\x22\x00\x00", // "C"
+        "\x00\x7F\x41\x41\x22\x1C\x00\x00", // "D"
+        "\x00\x7F\x49\x49\x49\x41\x00\x00", // "E"
+        "\x00\x7F\x09\x09\x09\x01\x00\x00", // "F"
+        "\x00\x3E\x41\x41\x51\x72\x00\x00", // "G"
+        "\x00\x7F\x08\x08\x08\x7F\x00\x00", // "H"
+        "\x00\x41\x7F\x41\x00\x00\x00\x00", // "I"
+        "\x00\x20\x40\x41\x3F\x01\x00\x00", // "J"
+        "\x00\x7F\x08\x14\x22\x41\x00\x00", // "K"
+        "\x00\x7F\x40\x40\x40\x40\x00\x00", // "L"
+        "\x00\x7F\x02\x0C\x02\x7F\x00\x00", // "M"
+        "\x00\x7F\x04\x08\x10\x7F\x00\x00", // "N"
+        "\x00\x3E\x41\x41\x41\x3E\x00\x00", // "O"
+        "\x00\x7F\x09\x09\x09\x06\x00\x00", // "P"
+        "\x00\x3E\x41\x51\x21\x5E\x00\x00", // "Q"
+        "\x00\x7F\x09\x19\x29\x46\x00\x00", // "R"
+        "\x00\x26\x49\x49\x49\x32\x00\x00", // "S"
+        "\x00\x01\x01\x7F\x01\x01\x00\x00", // "T"
+        "\x00\x3F\x40\x40\x40\x3F\x00\x00", // "U"
+        "\x00\x1F\x20\x40\x20\x1F\x00\x00", // "V"
+        "\x00\x3F\x40\x38\x40\x3F\x00\x00", // "W"
+        "\x00\x63\x14\x08\x14\x63\x00\x00", // "X"
+        "\x00\x03\x04\x78\x04\x03\x00\x00", // "Y"
+        "\x00\x61\x51\x49\x45\x43\x00\x00", // "Z"
+        "\x00\x7F\x41\x41\x00\x00\x00\x00", // """
+        "\x00\x02\x04\x08\x10\x20\x00\x00", // "\"
+        "\x00\x41\x41\x7F\x00\x00\x00\x00", // """
+        "\x00\x04\x02\x01\x02\x04\x00\x00", // "^"
+        "\x00\x80\x80\x80\x80\x80\x00\x00", // "_"
+        "\x00\x01\x02\x04\x00\x00\x00\x00", // "`"
+        "\x00\x20\x54\x54\x54\x78\x00\x00", // "a"
+        "\x00\x7F\x48\x44\x44\x38\x00\x00", // "b"
+        "\x00\x38\x44\x44\x28\x00\x00\x00", // "c"
+        "\x00\x38\x44\x44\x48\x7F\x00\x00", // "d"
+        "\x00\x38\x54\x54\x54\x18\x00\x00", // "e"
+        "\x00\x08\x7E\x09\x02\x00\x00\x00", // "f"
+        "\x00\x18\xA4\xA4\xA4\x7C\x00\x00", // "g"
+        "\x00\x7F\x08\x04\x04\x78\x00\x00", // "h"
+        "\x00\x00\x7D\x00\x00\x00\x00\x00", // "i"
+        "\x00\x80\x84\x7D\x00\x00\x00\x00", // "j"
+        "\x00\x7F\x10\x28\x44\x00\x00\x00", // "k"
+        "\x00\x41\x7F\x40\x00\x00\x00\x00", // "l"
+        "\x00\x7C\x04\x18\x04\x78\x00\x00", // "m"
+        "\x00\x7C\x08\x04\x7C\x00\x00\x00", // "n"
+        "\x00\x38\x44\x44\x38\x00\x00\x00", // "o"
+        "\x00\xFC\x24\x24\x18\x00\x00\x00", // "p"
+        "\x00\x18\x24\x24\xFC\x00\x00\x00", // "q"
+        "\x00\x00\x7C\x08\x04\x00\x00\x00", // "r"
+        "\x00\x48\x54\x54\x24\x00\x00\x00", // "s"
+        "\x00\x04\x7F\x44\x00\x00\x00\x00", // "t"
+        "\x00\x3C\x40\x40\x7C\x00\x00\x00", // "u"
+        "\x00\x1C\x20\x40\x20\x1C\x00\x00", // "v"
+        "\x00\x3C\x40\x30\x40\x3C\x00\x00", // "w"
+        "\x00\x44\x28\x10\x28\x44\x00\x00", // "x"
+        "\x00\x1C\xA0\xA0\x7C\x00\x00\x00", // "y"
+        "\x00\x44\x64\x54\x4C\x44\x00\x00", // "z"
+        "\x00\x08\x36\x41\x00\x00\x00\x00", // "{"
+        "\x00\x00\x7F\x00\x00\x00\x00\x00", // "|"
+        "\x00\x41\x36\x08\x00\x00\x00\x00", // "}"
+        "\x00\x02\x01\x01\x02\x01\x00\x00"  // "~"
+    ];
+}
---
+++ README.md
@@ -0,0 +1,34 @@
+# SSD1306 OLED 12864
+
+This is the MakeCode Package for SSD1306 OLED controller, based on the makecode library available
+
+## Table of Contents
+
+* [URL](#url)
+* [Summary](#summary)
+* [Blocks](#blocks)
+* [License](#license)
+
+## URL
+project URL: ```https://github.com/DFRobot/pxt-OLED```
+
+## Summary
+0.96-inch 128x64 resolution OLED display module, using SSD1360 driver, communicates with the 
+host through the I2C interface.
+
+## Blocks
+### 1.showUserNumber
+![image](https://github.com/DFRobot/pxt-OLED/blob/master/image/showNumber.png)<br>
+Displays a number on the OLED module with a newline.
+
+### 2.showUserText
+![image](https://github.com/DFRobot/pxt-OLED/blob/master/image/showUserText.png)<br>
+Displays a string on the OLED module with a newline.
+
+### 3.clear
+![image](https://github.com/DFRobot/pxt-OLED/blob/master/image/clear.png)<br>
+Clears the display.
+
+## License
+
+MIT
